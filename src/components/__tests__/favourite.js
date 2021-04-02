d8bcf3bae8ef0bf0728d4e8ab539cb94145a236a
bassamkdev <bassamkazemi.dev@gmail.com>
2021-04-01 22:15:40 -0700

    tested favourite icon component

---
+++ src/components/__tests__/favourite.js
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { appRender, fireEvent, waitFor, act } from "../../test/test-utils";
+import faker from 'faker'
+import { Favourite } from "../favourite";
+import { useFavourites } from "../../context/favourites.context";
+
+jest.mock('../../context/favourites.context')
+
+
+test('should call add function when is not set as favourtie ', async() => {
+    const mockAddRestaurantToFavourites = jest.fn()
+    const mockRemoveRestaurantFromFavourites = jest.fn()
+    const mockRestaurant = {
+        name: faker.lorem.word(),
+        icon: faker.internet.url(),
+        photos: [faker.internet.url()],
+        vicinity: faker.address.streetAddress(),
+        isOpenNow: true,
+        rating: 5,
+        isClosedTemporarily: true
+      } 
+
+    useFavourites.mockReturnValue({
+        favourites: [],
+        addRestaurantToFavourites: mockAddRestaurantToFavourites,
+        removeRestaurantFromFavourites: mockRemoveRestaurantFromFavourites,
+      })
+
+     const {getByA11yLabel, debug} = appRender(
+        <Favourite restaurant={mockRestaurant}/>
+    )
+    fireEvent.press(getByA11yLabel(/favourite icon/i))
+        expect(mockAddRestaurantToFavourites).toHaveBeenCalledTimes(1)
+        expect(mockRemoveRestaurantFromFavourites).not.toHaveBeenCalled()
+        debug()
+})
+
+test('should call remove function when is not set as favourtie ', async() => {
+    const mockAddRestaurantToFavourites = jest.fn()
+    const mockRemoveRestaurantFromFavourites = jest.fn()
+    const mockRestaurant = {
+        name: faker.lorem.word(),
+        icon: faker.internet.url(),
+        photos: [faker.internet.url()],
+        vicinity: faker.address.streetAddress(),
+        isOpenNow: true,
+        rating: 5,
+        isClosedTemporarily: true
+      } 
+
+    useFavourites.mockReturnValue({
+        favourites: {[mockRestaurant.name]: mockRestaurant},
+        addRestaurantToFavourites: mockAddRestaurantToFavourites,
+        removeRestaurantFromFavourites: mockRemoveRestaurantFromFavourites,
+      })
+
+     const {getByA11yLabel} = appRender(
+        <Favourite restaurant={mockRestaurant}/>
+    )
+    fireEvent.press(getByA11yLabel(/favourite icon/i))
+        expect(mockRemoveRestaurantFromFavourites).toHaveBeenCalledTimes(1)
+        expect(mockAddRestaurantToFavourites).not.toHaveBeenCalled()
+})
--- src/components/favourites-bar.js
+++ src/components/favourites-bar.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import styled from '@emotion/native'
-import {Text, ScrollView} from 'react-native'
+import {ScrollView} from 'react-native'
 import {useFavourites} from '../context/favourites.context'
 import {Callout} from './map-callout'
 
